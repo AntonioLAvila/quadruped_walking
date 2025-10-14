b99c69e32fc4c4ae470947ce66718962987c84a5
Antonio Avila <antonioavila433@gmail.com>
2025-10-13 23:03:23 -0400

    refactor

--- env.py
+++ env.py
@@ -3,33 +3,34 @@ from pydrake.all import (
     AddMultibodyPlantSceneGraph,
     Parser,
     Simulator,
-    FindResourceOrThrow,
     AddDefaultVisualization,
     RigidTransform,
     RollPitchYaw,
     CoulombFriction,
     HalfSpace,
-    RobotDiagramBuilder,
     Diagram,
     MultibodyPlant,
     ModelInstanceIndex,
-    StartMeshcat
+    StartMeshcat,
+    SceneGraph,
+    Meshcat,
+    MeshcatVisualizer
 )
 import numpy as np
 
 
-def make_a1(meshcat) -> tuple[Diagram, MultibodyPlant, ModelInstanceIndex]:
+def make_a1(meshcat: Meshcat) -> tuple[Diagram, MultibodyPlant, ModelInstanceIndex]:
     '''
     returns the a1 diagram and a hook for the a1
     '''
-    robot_builder = RobotDiagramBuilder(time_step=1e-4)
-    plant = robot_builder.plant()
-    scene_graph = robot_builder.scene_graph()
-    parser = robot_builder.parser()
-    builder = robot_builder.builder()
-    
-    parser = Parser(plant)
-    a1_model = parser.AddModels('assets/a1.urdf')[0]
+    builder = DiagramBuilder()
+
+    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=1e-4)
+    plant: MultibodyPlant = plant
+    scene_graph: SceneGraph = scene_graph
+
+    parser = Parser(plant, scene_graph)
+    a1 = parser.AddModels('assets/a1.urdf')[0]
 
     plant.RegisterCollisionGeometry(
         plant.world_body(),
@@ -38,47 +39,39 @@ def make_a1(meshcat) -> tuple[Diagram, MultibodyPlant, ModelInstanceIndex]:
         "ground_collision",
         CoulombFriction(1.0, 1.0),
     )
-    plant.RegisterVisualGeometry(
-        plant.world_body(),
-        HalfSpace.MakePose(np.array([0, 0, 1]), np.zeros(3)),
-        HalfSpace(),
-        "ground_visual",
-        [0.5, 0.5, 0.5, 1.0],
-    )
+    
+    MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
 
     plant.Finalize()
+    diagram = builder.Build()
 
-    AddDefaultVisualization(builder, meshcat=meshcat)
+    return diagram, plant, a1
 
-    diagram = robot_builder.Build()
+if __name__ == '__main__':
+    meshcat: Meshcat = StartMeshcat()
 
-    return diagram, plant, a1_model
+    diagram, plant, a1_model = make_a1(meshcat)
+    simulator = Simulator(diagram)
 
+    context = simulator.get_mutable_context()
+    plant_context = plant.GetMyContextFromRoot(context)
 
-meshcat = StartMeshcat()
+    initial_pose = RigidTransform(
+        RollPitchYaw(0, 0, 0),   # no rotation
+        [0, 0, 0.3]              # x, y, z position
+    )
+    plant.SetFreeBodyPose(
+        plant_context,
+        plant.GetBodyByName("trunk", a1_model),
+        initial_pose
+    )
 
-diagram, plant, a1_model = make_a1(meshcat)
-simulator = Simulator(diagram)
+    simulator.set_target_realtime_rate(1.0)
+    print("🌐 Open Meshcat at: http://localhost:7000")
 
-context = simulator.get_mutable_context()
-plant_context = plant.GetMyContextFromRoot(context)
+    meshcat.StartRecording()
+    simulator.AdvanceTo(5.0)
+    meshcat.PublishRecording()
 
-initial_pose = RigidTransform(
-    RollPitchYaw(0, 0, 0),   # no rotation
-    [0, 0, 0.3]              # x, y, z position
-)
-plant.SetFreeBodyPose(
-    plant_context,
-    plant.GetBodyByName("trunk", a1_model),
-    initial_pose
-)
-
-simulator.set_target_realtime_rate(1.0)
-print("🌐 Open Meshcat at: http://localhost:7000")
-
-meshcat.StartRecording()
-simulator.AdvanceTo(5.0)
-meshcat.PublishRecording()
-
-while True:
-    pass
+    while True:
+        pass
