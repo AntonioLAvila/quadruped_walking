9c9f501d9afa7460c3ca032b1a5277f872936515
Antonio Avila <antonioavila433@gmail.com>
2025-10-20 13:44:20 -0400

    works but need to add noise and randomized conditions

--- env.py
+++ env.py
@@ -22,17 +22,19 @@ import numpy as np
 from pydrake.gym import DrakeGymEnv
 from gymnasium.spaces import Box
 from stable_baselines3 import PPO
+from util import ObservationExtractor
 
 def make_a1(meshcat: Meshcat) -> tuple[Diagram, MultibodyPlant, ModelInstanceIndex]:
     builder = DiagramBuilder()
-
     plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=1e-4)
     plant: MultibodyPlant = plant
     scene_graph: SceneGraph = scene_graph
-
     parser = Parser(plant, scene_graph)
+
+    # add a1
     a1 = parser.AddModels('assets/a1.urdf')[0]
 
+    # register floor
     plant.RegisterCollisionGeometry(
         plant.world_body(),
         HalfSpace.MakePose(np.array([0, 0, 1]), np.zeros(3)),
@@ -41,13 +43,19 @@ def make_a1(meshcat: Meshcat) -> tuple[Diagram, MultibodyPlant, ModelInstanceInd
         CoulombFriction(1.0, 1.0),
     )
     
+    # visualizer
     MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
 
     plant.Finalize()
 
+    # wire up observation/actuation
     builder.ExportInput(plant.get_actuation_input_port(), 'actuation') # 12
-    builder.ExportOutput(plant.get_state_output_port(), 'state') # 37
+    observation_extractor = ObservationExtractor(plant)
+    builder.AddNamedSystem('observation_extractor', observation_extractor)
+    builder.Connect(plant.get_state_output_port(), observation_extractor.input_port)
+    builder.ExportOutput(observation_extractor.output_port) # 30
 
+    # contact forces
     ContactVisualizer.AddToBuilder(builder, plant, meshcat)
 
     diagram = builder.Build()
@@ -59,7 +67,7 @@ def make_simulation_maker(meshcat: Meshcat):
     def simulation_factory(generator: RandomGenerator) -> Simulator:
         diagram, plant, a1 = make_a1(meshcat)
         simulator = Simulator(diagram)
-        # TODO randomize stuff
+        # TODO randomize stuff agraagaragragragragragragrgragragra
         return simulator
     
     return simulation_factory
@@ -73,6 +81,10 @@ if __name__ == '__main__':
 
     diagram, plant, a1 = make_a1(meshcat)
 
+    # print(plant.num_positions(), plant.GetPositionNames())
+    # print(plant.num_velocities(), plant.GetVelocityNames())
+    # print(plant.num_multibody_states())
+
     action_space = Box(
         low=plant.GetEffortLowerLimits(),
         high=plant.GetEffortUpperLimits(),
@@ -80,8 +92,14 @@ if __name__ == '__main__':
     )
 
     observation_space = Box(
-        low=plant.GetPositionLowerLimits(),
-        high=plant.GetPositionUpperLimits(),
+        low=np.concatenate((
+            plant.GetPositionLowerLimits()[7:],
+            plant.GetVelocityLowerLimits()
+        )),
+        high=np.concatenate((
+            plant.GetPositionUpperLimits()[7:],
+            plant.GetVelocityUpperLimits()
+        )),
         dtype=np.float64
     )
 
@@ -92,6 +110,9 @@ if __name__ == '__main__':
         observation_space=observation_space,
         reward=reward
     )
+
+    env.reset()
+    env.render()
 
     model = PPO('MlpPolicy', env, verbose=1)
 
--- util.py
+++ util.py
@@ -5,14 +5,15 @@ from pydrake.all import (
 )
 
 class ObservationExtractor(LeafSystem):
+    # TODO add noise argagrgagrgragargragragaargargarrgrggrag
     def __init__(self, plant: MultibodyPlant):
         super().__init__()
         self.plant = plant
-        obs_dim = plant.num_positions() + plant.num_velocities()
-        self.DeclareVectorInputPort("state", plant.num_multibody_states())
-        self.DeclareVectorOutputPort("observation", BasicVector(obs_dim), self.calc_obs)
+        obs_dim = 12 + 12 + 3 + 3 # NOTE 12 joint positions, 12 joint velocities, 3 body translational, 3 body rotational velocities
+        self.input_port = self.DeclareVectorInputPort("state", plant.num_multibody_states())
+        self.output_port = self.DeclareVectorOutputPort("observation", BasicVector(obs_dim), self.calc_obs)
 
     def calc_obs(self, context, output):
         x = self.get_input_port().Eval(context)
-        q = x[:self.plant.num_positions()]
-        output.SetFromVector(q)
+        obs = x[7:]
+        output.SetFromVector(obs)
