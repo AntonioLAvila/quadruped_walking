db174e2308eabf5e237246db7c60d2b9f8ef1895
Antonio Avila <antonioavila433@gmail.com>
2025-10-21 17:37:29 -0400

    guh

--- train.py
+++ train.py
@@ -1,23 +1,17 @@
 from env import make_simulation_maker, reward_fn, make_gym_env
 from stable_baselines3 import PPO
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    env = make_gym_env(reward_fn, make_simulation_maker)
+    parser = ArgumentParser()
+    parser.add_argument('--num_steps', type=int, required=True)
+    args = parser.parse_args()
+    args = vars(args)
 
-    # env.reset()
-    # env.render()
-    
+    env = make_gym_env(reward_fn, make_simulation_maker)
+   
     model = PPO('MlpPolicy', env, verbose=1)
 
-    model.learn(total_timesteps=800000, progress_bar=True)
+    model.learn(total_timesteps=args['num_steps'], progress_bar=True)
 
     model.save('ppo_A1_final')
-
-
-    # obs, _ = env.reset()
-    # while True:
-    #     action, _states = model.predict(obs, deterministic=True)
-    #     obs, reward, terminated, truncated, info = env.step(action)
-    #     if terminated or truncated:
-    #         print("Episode finished. Resetting.")
-    #         obs, _ = env.reset()
