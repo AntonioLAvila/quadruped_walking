32b87c18011abb3a96aad276464c6d40e1238454
Antonio Avila <antonioavila433@gmail.com>
2025-11-07 22:37:41 -0500

    require paths now dont save ckpts to this folder
    should follow this convention for ease of development

--- quick_train.sh
+++ quick_train.sh
@@ -1,2 +1,1 @@
-# export CUDA_VISIBLE_DEVICES=
-python train.py --num_envs 12 --num_steps 100000000 --model_dir "buh" --eval_freq 5000000
+python train.py --num_envs 12 --num_steps 100000000 --model_dir "$HOME/ckpts" --eval_freq 5000000
